a4c93d80d8bc8e6e47413ca723cfae1b25b8f74b
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-18 14:59:09 +0900

    ch02_10_class_constructor

---
+++ dart_project/ch02_10_class_constructor/bin/ch02_10_class_constructor.dart
@@ -0,0 +1,35 @@
+void main() {
+  print('class 공부');
+  // instance 인스턴스: object(객체) 생성
+  Student girlStudent = Student(
+    name: '나연',
+    age: 22,
+  );
+  print(girlStudent.name);
+  print(girlStudent.age);
+  girlStudent.printInfo();
+
+  girlStudent.name = '보라';
+  girlStudent.age = 24;
+  girlStudent.printInfo();
+}
+
+// Class(클래스): 객체지향프로그래밍(OOP)에서 특정 object(객체)를 생성하기 위해 정의하는 일종의 틀
+// - class는 변수와 함수를 정의할 수 있다.
+// - 비슷한 성격을 가진 연관있는 변수와 함수들을 한 class에 정의합니다.
+
+// class 클래스명 {}
+class Student {
+  // 생성자 contructor
+  // Student();
+  Student({required String name, this.age}) : name = '$name 학생';
+
+  String name = 'bora';
+  int? age;
+
+  void printInfo() {
+    print('-----');
+    print('name: $name');
+    print('age: $age');
+  }
+}
---
+++ dart_project/ch02_10_class_constructor/test/ch02_10_class_constructor_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch02_10_class_constructor/ch02_10_class_constructor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
