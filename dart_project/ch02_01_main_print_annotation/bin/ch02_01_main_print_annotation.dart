1eb950a0ca3fb224d17f2e3b00e3460960351a60
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-22 10:16:23 +0900

    ch02_01_main_print_annotation 복습

--- dart_project/ch02_01_main_print_annotation/bin/ch02_01_main_print_annotation.dart
+++ dart_project/ch02_01_main_print_annotation/bin/ch02_01_main_print_annotation.dart
@@ -1,9 +1,12 @@
-// 1. main 함수 형태 void main(){}
-// main 함수 무조건 있어야 한다.
+// ch02_01_main_print_annotation
+
+// 1. main 함수 형태
+// void main() {}
+// main 함수는 무조건 있어야 한다.
 // (main 함수 기준 실행)
 
 void main() {
-  // 2. print는 ()안에 작성되어있는 object요소를 콘솔창 출력하고 싶을 때 사용한다.
+  // 2. print는 () 안에 작성되어있는 object 요소를 콘솔창에 출력하고 싶을 때 사용한다.
   print('Hello World');
   print('object Hello World');
   print('object @@ World');
@@ -15,6 +18,6 @@ void main() {
   print(5 / 2);
 
   // 3. 주석
-  //  (1) //
-  //  (2) cmd + /
+  // (1) //
+  // (2) cmd + /
 }
