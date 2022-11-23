3ef7a04ea7469d6eb71017a8bc2215ee93d5c376
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-23 14:10:03 +0900

    ch01_14_assetimage_networkimage

--- drawing_app_widget/counting_sample/lib/main.dart
+++ drawing_app_widget/counting_sample/lib/main.dart
@@ -1,5 +1,5 @@
-// ch01_13_constructor
-// 위젯간 데이터 전달(생성자)
+// ch01_14_assetimage_networkimage
+// 이미지 출력 위젯 작성
 
 import 'package:flutter/material.dart';
 
@@ -16,47 +16,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(
-        name: 'Junyeong',
-        title: 'title',
-      ),
+      home: const _ImageHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
+class _ImageHomePage extends StatelessWidget {
+  const _ImageHomePage({
     Key? key,
-    required this.title,
-    required this.name,
   }) : super(key: key);
 
-  final String title;
-  final String name;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Hello ${widget.title}!',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'Hello ${widget.name} World',
+            Image.asset(
+              'assets/images/image.jpeg',
+              width: 300,
+              height: 300,
+              fit: BoxFit.none,
             ),
           ],
         ),
       ),
     );
+    // 1. asset(로컬 이미지 표시)
+    // - pub.yaml 파일 입력
+    // - 단일 파일 인식, 폴더 인식하는 것 차이 설명
+    // Image.asset('assets/images/image.jpeg'),
+    // Image.asset('assets/images/image3.jpeg'),
+    // Image.asset('assets/images/image2.gif'),
+    //
+    // 2. network(네트워크 이미지 표시)
+    // - 플러터가 이미지 파일을 다운로드해서 캐시에 저장하고 화면에 띄움
+    // asset 이미지보다 로딩이 느림
+    // 인터넷 연결 필요
+    // Image.network(
+    //             'https://thumbs.gfycat.com/ArcticSameGoldenmantledgroundsquirrel-size_restricted.gif'),
+    // Image.network(
+    //             'https://storage.googleapis.com/jjalbot/2021/12/h-ckJpdv5/h-ckJpdv5.jpeg'),
+    //
+    // 3. file(파일 이미지 표시)
+    // - 외부 폴더나 갤러리에 있는 이미지를 가져올 땐 File type
+    // Image.file(file),
+    // # 공통된 속성
+    // - width, height
+    // - BoxFit
   }
 }
