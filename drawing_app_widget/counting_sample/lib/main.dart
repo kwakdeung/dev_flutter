01a243f1620769d55a6d2fb7aef723d3b512ad84
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-23 15:47:22 +0900

    ch02_02_material_android_style_widget_explanation

--- drawing_app_widget/counting_sample/lib/main.dart
+++ drawing_app_widget/counting_sample/lib/main.dart
@@ -1,5 +1,5 @@
-// ch02_01_remainder_existing_widget_explanation
-// 나머지 기존 위젯 설명
+// ch02_02_material_android_style_widget_explanation
+// 머터리얼 안드로이드 스타일 위젯 설명
 
 import 'package:flutter/material.dart';
 
@@ -21,80 +21,68 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class _ImageHomePage extends StatelessWidget {
+class _ImageHomePage extends StatefulWidget {
   const _ImageHomePage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<_ImageHomePage> createState() => _ImageHomePageState();
+}
+
+class _ImageHomePageState extends State<_ImageHomePage> {
+  final TextEditingController _text = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('title'),
+        title: Text('${_text.text}'),
       ),
-      body: const Center(
-        child: Placeholder(),
+      body: Center(
+        child: TextField(
+          controller: _text,
+        ),
       ),
     );
   }
-  // AppBar
-  // appBar: AppBar(
-  //   title: const Text('AppBar Demo'),
-  //   actions: <Widget>[
-  //     IconButton(
-  //       icon: const Icon(Icons.add_alert),
-  //       tooltip: 'Show Snackbar',
-  //       onPressed: () {
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //             const SnackBar(content: Text('This is a snackbar')));
-  //       },
-  //     ),
-  //     IconButton(
-  //       icon: const Icon(Icons.navigate_next),
-  //       tooltip: 'Go to the next page',
-  //       onPressed: () {
-  //         Navigator.push(context, MaterialPageRoute<void>(
-  //           builder: (BuildContext context) {
-  //             return Scaffold(
-  //               appBar: AppBar(
-  //                 title: const Text('Next page'),
-  //               ),
-  //               body: const Center(
-  //                 child: Text(
-  //                   'This is the next page',
-  //                   style: TextStyle(fontSize: 24),
-  //                 ),
-  //               ),
-  //             );
-  //           },
-  //         ));
-  //       },
-  //     ),
-  //   ],
-  // ),
+  // 머터리얼 안드로이드 스타일 위젯 설명
+  // 1. bottomNavigationBar
+  // int _index = 2;
   //
-  // FlutterLogo
-  // FlutterLogo(
-  //   size: 300,
-  // ),
-  // Icon
-  // Icon(
-  //         Icons.star,
-  //         size: 200,
+  // bottomNavigationBar: BottomNavigationBar(
+  //       currentIndex: _index,
+  //       onTap: (value) => setState(() {
+  //         _index = value;
+  //       }),
+  //       items: const <BottomNavigationBarItem>[
+  //         BottomNavigationBarItem(icon: Icon(Icons.star), label: 'star'),
+  //         BottomNavigationBarItem(
+  //             icon: Icon(Icons.access_alarm), label: 'access_alarm'),
+  //         BottomNavigationBarItem(
+  //             icon: Icon(Icons.settings), label: 'settings'),
+  //       ],
+  //     ),
+  //
+  // 2. Slider
+  // double _slider = 0.3;
+  //
+  // Slider(
+  //         value: _slider,
+  //         onChanged: ((value) => setState(() {
+  //               _slider = value;
+  //             })),
   //       ),
   //
-  // Image
-  // Image(
-  //  image: AssetImage(),
-  // )
-  // Image(
-  //  image: NetworkImage(),
-  // )
-  //
-  // Placeholder
-  // 다른 위젯이 언젠가 추가될 위치를 나타내는 상자를 그리는 위젯
-  // 개발 중에 인터페이스가 아직 완료되지 않았음을 나타내는 데 유용함.
-  // Placeholder(),
-  //
-  // Scaffold: 머터리얼 디자인 시각적 레이아웃 구조
+  // 3. TextField
+  // Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('${_text.text}'),
+  //     ),
+  //     body: Center(
+  //       child: TextField(
+  //         controller: _text,
+  //       ),
+  //     ),
+  //   );
 }
