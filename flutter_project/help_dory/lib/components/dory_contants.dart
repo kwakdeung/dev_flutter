63cd6e0301d74d495b2a714cd5fc3509cacced3e
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-25 11:18:56 +0900

    ch02_02_create_simple_ui_2_and_widget_detail
    *main.dart
    16 라인 우측 상단 debug 없애기
    
    *dory_contants.dart
    상수 생성
    
    *dory_themes.dart
    테마 상수 생성
    11, 23~29 라인 appBarTheme 생성
    
    *add_medicine_page.dart
    4 라인 import 추가 dory_contants.dart
    14, 63 라인 약 이름 텍스트 컨트롤러 추가_nameController
    17 라인 dispose 처리 _nameController
    32 라인 padding 추가 - pagePadding
    42~43 라인 margin 추가 - top: largeSpace, bottom: largeSpace + regularSpace
    62~73 라인 TextFormField 추가 - 복용할 약 이름 기입란
    79~80 라인 버튼 Padding 조절 - submitButtonBoxPadding
    81~82 라인 height 추가 submitButtonHeight

---
+++ flutter_project/help_dory/lib/components/dory_contants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const double regularSpace = 20.0;
+
+const double largeSpace = 40.0;
+
+const double smallSpace = 10.0;
+
+const double submitButtonHeight = 48.0;
+
+const pagePadding = EdgeInsets.all(20.0);
+
+const EdgeInsetsGeometry textFieldContentPadding =
+    EdgeInsets.symmetric(horizontal: 6);
+
+const EdgeInsetsGeometry submitButtonBoxPadding = EdgeInsets.symmetric(
+  horizontal: 20,
+  vertical: 10,
+);
--- flutter_project/help_dory/lib/pages/add_medicine/add_medicine_page.dart
+++ flutter_project/help_dory/lib/pages/add_medicine/add_medicine_page.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AddMedicinePage extends StatelessWidget {
+import '../../components/dory_contants.dart';
+
+class AddMedicinePage extends StatefulWidget {
   const AddMedicinePage({super.key});
+
+  @override
+  State<AddMedicinePage> createState() => _AddMedicinePageState();
+}
+
+class _AddMedicinePageState extends State<AddMedicinePage> {
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,41 +29,65 @@ class AddMedicinePage extends StatelessWidget {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '어떤 약이에요?',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Center(
-              child: CircleAvatar(
-                radius: 40,
-                child: CupertinoButton(
-                  onPressed: () {},
-                  child: const Icon(
-                    CupertinoIcons.photo_camera_solid,
-                    size: 30,
-                    color: Colors.white,
+        child: Padding(
+          padding: pagePadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '어떤 약이에요?',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                    top: largeSpace, bottom: largeSpace + regularSpace),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 40,
+                    child: CupertinoButton(
+                      onPressed: () {},
+                      child: const Icon(
+                        CupertinoIcons.photo_camera_solid,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Text(
-              '약 이름',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            TextFormField(),
-          ],
+              Text(
+                '약 이름',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              TextFormField(
+                controller: _nameController,
+                maxLength: 20,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                style: Theme.of(context).textTheme.bodyText1,
+                decoration: InputDecoration(
+                  hintText: '복용할 약 이름을 기입해주세요.',
+                  hintStyle: Theme.of(context).textTheme.bodyText2,
+                  contentPadding: textFieldContentPadding,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: SafeArea(
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.subtitle1,
+        child: Padding(
+          padding: submitButtonBoxPadding,
+          child: SizedBox(
+            height: submitButtonHeight,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.subtitle1,
+              ),
+              child: const Text('다음'),
+            ),
           ),
-          child: const Text('다음'),
         ),
       ),
     );
