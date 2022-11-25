767c8d4aa060a3302c4f493446e3d241ab886f56
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-25 17:46:03 +0900

    ch03_03_implementation_screen_function_1
    *pubspec.yaml
    17 라인 패키지 추가 intl
    
    *add_alarm_page.dart
    8 라인 패키지 추가 intl
    11 라인 AddAlarmPage 클래스로 정리
    23~27 라인 기본 알림 시간 설정
    44 라인 get alarmWidgets으로 변경
    62 라인 get alarmWidgets - map 활용
    90~96 라인 생성자
    101 라인 초기 시간
    137 라인 생성자 initialDateTime
    153 라인 생성자 호출 initialDateTime
    166, 181 라인 textStyle 변경
    194, 198 라인 생성자 콜백함수 onPressed
    208 라인 onPressed 호출

--- flutter_project/help_dory/lib/pages/add_medicine/add_alarm_page.dart
+++ flutter_project/help_dory/lib/pages/add_medicine/add_alarm_page.dart
@@ -5,14 +5,26 @@ import 'package:flutter/material.dart';
 import 'package:help_dory/components/dory_colors.dart';
 import 'package:help_dory/components/dory_contants.dart';
 import 'package:help_dory/components/dory_widgets.dart';
+import 'package:intl/intl.dart';
 import 'components/add_page_widget.dart';
 
-class AddAlarmPage extends StatelessWidget {
+class AddAlarmPage extends StatefulWidget {
   final File? medicineImage;
   final String medicineName;
 
   const AddAlarmPage(
       {super.key, required this.medicineImage, required this.medicineName});
+
+  @override
+  State<AddAlarmPage> createState() => _AddAlarmPageState();
+}
+
+class _AddAlarmPageState extends State<AddAlarmPage> {
+  final _alarms = <String>{
+    '08:00',
+    '13:00',
+    '19:00',
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +41,13 @@ class AddAlarmPage extends StatelessWidget {
           ),
           Expanded(
             child: ListView(
-              children: const [
-                AlarmBox(),
-                AlarmBox(),
-                AlarmBox(),
-                AddAlarmButton(),
-              ],
+              children: alarmWidgets,
+              // children: const [
+              //   AlarmBox(),
+              //   AlarmBox(),
+              //   AlarmBox(),
+              //   AddAlarmButton(),
+              // ],
             ),
           ),
         ],
@@ -45,21 +58,54 @@ class AddAlarmPage extends StatelessWidget {
       ),
     );
   }
+
+  List<Widget> get alarmWidgets {
+    final children = <Widget>[];
+    children.addAll(
+      _alarms.map(
+        (alarmTime) => AlarmBox(
+          time: alarmTime,
+          onPressedMinus: () {
+            setState(() {
+              _alarms.remove(alarmTime);
+            });
+          },
+        ),
+      ),
+    );
+    children.add(AddAlarmButton(
+      onPressed: () {
+        final now = DateTime.now();
+        final nowTime = DateFormat('HH:mm').format(now);
+        setState(() {
+          _alarms.add(nowTime);
+        });
+      },
+    ));
+    return children;
+  }
 }
 
 class AlarmBox extends StatelessWidget {
+  final String time;
+  final VoidCallback onPressedMinus;
+
   const AlarmBox({
     Key? key,
+    required this.time,
+    required this.onPressedMinus,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final initTime = DateFormat('HH:mm').parse(time);
+
     return Row(
       children: [
         Expanded(
           flex: 1,
           child: IconButton(
-            onPressed: () {},
+            onPressed: onPressedMinus,
             icon: const Icon(CupertinoIcons.minus_circle),
           ),
         ),
@@ -73,55 +119,13 @@ class AlarmBox extends StatelessWidget {
               showModalBottomSheet(
                 context: context,
                 builder: (context) {
-                  return BottomSheetBody(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: CupertinoDatePicker(
-                          onDateTimeChanged: (dateTime) {},
-                          mode: CupertinoDatePickerMode.time,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: regularSpace,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                                height: submitButtonHeight,
-                                child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      textStyle:
-                                          Theme.of(context).textTheme.subtitle1,
-                                      backgroundColor: Colors.white,
-                                      foregroundColor: DoryColors.primaryColor,
-                                    ),
-                                    onPressed: () {},
-                                    child: Text('취소'))),
-                          ),
-                          const SizedBox(
-                            width: smallSpace,
-                          ),
-                          Expanded(
-                            child: SizedBox(
-                                height: submitButtonHeight,
-                                child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      textStyle:
-                                          Theme.of(context).textTheme.subtitle1,
-                                    ),
-                                    onPressed: () {},
-                                    child: Text('선택'))),
-                          ),
-                        ],
-                      )
-                    ],
+                  return TimePickerBottomSheet(
+                    initialDateTime: initTime,
                   );
                 },
               );
             },
-            child: const Text('20:00'),
+            child: Text(time),
           ),
         ),
       ],
@@ -129,9 +133,69 @@ class AlarmBox extends StatelessWidget {
   }
 }
 
+class TimePickerBottomSheet extends StatelessWidget {
+  final DateTime initialDateTime;
+
+  const TimePickerBottomSheet({
+    Key? key,
+    required this.initialDateTime,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheetBody(
+      children: [
+        SizedBox(
+          height: 200,
+          child: CupertinoDatePicker(
+            onDateTimeChanged: (dateTime) {},
+            mode: CupertinoDatePickerMode.time,
+            initialDateTime: initialDateTime,
+          ),
+        ),
+        const SizedBox(
+          height: regularSpace,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                  height: submitButtonHeight,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        textStyle: Theme.of(context).textTheme.subtitle1,
+                        backgroundColor: Colors.white,
+                        foregroundColor: DoryColors.primaryColor,
+                      ),
+                      onPressed: () {},
+                      child: Text('취소'))),
+            ),
+            const SizedBox(
+              width: smallSpace,
+            ),
+            Expanded(
+              child: SizedBox(
+                  height: submitButtonHeight,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        textStyle: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      onPressed: () {},
+                      child: Text('선택'))),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
 class AddAlarmButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
   const AddAlarmButton({
     Key? key,
+    required this.onPressed,
   }) : super(key: key);
 
   @override
@@ -141,7 +205,7 @@ class AddAlarmButton extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
         textStyle: Theme.of(context).textTheme.subtitle1,
       ),
-      onPressed: () {},
+      onPressed: onPressed,
       child: Row(
         children: const [
           Expanded(
