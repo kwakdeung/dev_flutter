bff4f92a52d84b95a836decb86b052c791bd7e8f
kwakdeung <rhkrwnsdud22@naver.com>
2022-11-29 10:55:28 +0900

    테마 디자인 수정

--- flutter_project/dory/lib/components/dory_themes.dart
+++ flutter_project/dory/lib/components/dory_themes.dart
@@ -8,25 +8,16 @@ class DoryThemes {
         scaffoldBackgroundColor: Colors.white,
         splashColor: Colors.white,
         textTheme: _textTheme,
-        appBarTheme: _appBarTheme,
         brightness: Brightness.light,
       );
 
-  static ThemeData get dartTheme => ThemeData(
+  static ThemeData get darkTheme => ThemeData(
         primarySwatch: DoryColors.primaryMeterialColor,
         fontFamily: 'GmarketSansTTF',
         splashColor: Colors.white,
         textTheme: _textTheme,
         brightness: Brightness.dark,
       );
-
-  static const AppBarTheme _appBarTheme = AppBarTheme(
-    backgroundColor: Colors.white,
-    iconTheme: IconThemeData(
-      color: DoryColors.primaryColor,
-    ),
-    elevation: 0,
-  );
 
   static const TextTheme _textTheme = TextTheme(
     headline4: TextStyle(
