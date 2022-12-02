326bb91efc5e80ffe2341de51431566be7b3932a
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-02 10:16:40 +0900

    ch02_09_class_declaration_instance 복습

--- dart_project/ch02_09_class_declaration_instance/bin/ch02_09_class_declaration_instance.dart
+++ dart_project/ch02_09_class_declaration_instance/bin/ch02_09_class_declaration_instance.dart
@@ -1,16 +1,18 @@
+// ch02_09_class_declaration_instance
+
 void main() {
   print('class 공부');
   // instance 인스턴스: object(객체) 생성
-  Student girlStudent = Student();
-  print(girlStudent.name);
-  print(girlStudent.age);
-  girlStudent.printInfo();
+  Student boyStudent = Student();
+  print(boyStudent.name);
+  print(boyStudent.age);
+  boyStudent.printInfo();
 
-  girlStudent.name = '보라';
-  girlStudent.age = 24;
-  print(girlStudent.name);
-  print(girlStudent.age);
-  girlStudent.printInfo();
+  boyStudent.name = '준영';
+  boyStudent.age = 30;
+  print(boyStudent.name);
+  print(boyStudent.age);
+  boyStudent.printInfo();
 }
 
 // Class(클래스): 객체지향프로그래밍(OOP)에서 특정 object(객체)를 생성하기 위해 정의하는 일종의 틀
@@ -19,7 +21,7 @@ void main() {
 
 // class 클래스명 {}
 class Student {
-  String name = 'bora';
+  String name = 'Junyeong';
   int? age;
 
   void printInfo() {
