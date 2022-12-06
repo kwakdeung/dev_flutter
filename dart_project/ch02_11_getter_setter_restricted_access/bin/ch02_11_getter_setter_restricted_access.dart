249210a1d545778fb289d98d37f9aeca78e2e756
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-06 11:26:41 +0900

    ch02_11_getter_setter_restricted_access 복습

--- dart_project/ch02_11_getter_setter_restricted_access/bin/ch02_11_getter_setter_restricted_access.dart
+++ dart_project/ch02_11_getter_setter_restricted_access/bin/ch02_11_getter_setter_restricted_access.dart
@@ -1,14 +1,16 @@
+// ch02_11_getter_setter_restricted_access
+
 import 'class/student.dart';
 
 void main() {
   print('class 공부');
   // instance 인스턴스: object(객체) 생성
-  Student girlStudent = Student(
-    name: '나연',
-    age: 22,
+  Student boyStudent = Student(
+    name: '준영',
+    age: 30,
   );
-  girlStudent.printInfo();
+  boyStudent.printInfo();
 
-  girlStudent.age = 24;
-  girlStudent.printInfo();
+  boyStudent.age = 24;
+  boyStudent.printInfo();
 }
