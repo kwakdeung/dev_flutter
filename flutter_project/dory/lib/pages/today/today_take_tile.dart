9ec8e697b6d7a14acac0cd25237937c663e93ad9
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-06 15:17:45 +0900

    ch06_05 _today_tile_icon_set_sorted_chronologically_empty_history_page

--- flutter_project/dory/lib/pages/today/today_take_tile.dart
+++ flutter_project/dory/lib/pages/today/today_take_tile.dart
@@ -304,6 +304,7 @@ class MedicineImageButton extends StatelessWidget {
       child: CircleAvatar(
         radius: 40,
         foregroundImage: imagePath == null ? null : FileImage(File(imagePath!)),
+        child: imagePath == null ? Icon(CupertinoIcons.alarm_fill) : null,
       ),
     );
   }
