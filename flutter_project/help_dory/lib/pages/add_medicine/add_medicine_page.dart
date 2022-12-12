308beda52d18d56e85c11c390958b36cc1c09a63
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-12 16:47:40 +0900

    ch02_03 _import_photos_from_gallery_add_function_take_photos
    add_medicine_page.dart
    
    1 라인 import 추가 File
    6 라인 import 추가 image_picker
    17 라인 File? _pickedImage
    50 라인 사진 찍기 + 불러오기

--- flutter_project/help_dory/lib/pages/add_medicine/add_medicine_page.dart
+++ flutter_project/help_dory/lib/pages/add_medicine/add_medicine_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:help_dory/components/dory_constants.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddMedicinePage extends StatefulWidget {
   const AddMedicinePage({super.key});
@@ -11,6 +14,7 @@ class AddMedicinePage extends StatefulWidget {
 
 class _AddMedicinePageState extends State<AddMedicinePage> {
   final _nameController = TextEditingController();
+  File? _pickedImage;
 
   @override
   void dispose() {
@@ -42,12 +46,27 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                 child: CircleAvatar(
                   radius: 40,
                   child: CupertinoButton(
-                    onPressed: () {},
-                    child: const Icon(
-                      CupertinoIcons.photo_camera_solid,
-                      size: 30,
-                      color: Colors.white,
-                    ),
+                    onPressed: () {
+                      ImagePicker()
+                          .pickImage(source: ImageSource.camera)
+                          .then((xfile) {
+                        if (xfile == null) return;
+                        setState(() {
+                          _pickedImage = File(xfile.path);
+                        });
+                      });
+                    },
+                    padding: _pickedImage == null ? null : EdgeInsets.zero,
+                    child: _pickedImage == null
+                        ? const Icon(
+                            CupertinoIcons.photo_camera_solid,
+                            size: 30,
+                            color: Colors.white,
+                          )
+                        : CircleAvatar(
+                            foregroundImage: FileImage(_pickedImage!),
+                            radius: 40,
+                          ),
                   ),
                 ),
               ),
