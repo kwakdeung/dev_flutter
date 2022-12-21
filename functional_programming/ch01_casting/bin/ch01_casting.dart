b13f6741cdc272b38c12ea8b935f142931bb6f7b
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-21 14:18:01 +0900

    ch01_casting

---
+++ functional_programming/ch01_casting/bin/ch01_casting.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<String> blackPink = ['로제', '지수', '리사', '제니', '제니'];
+
+  print(blackPink);
+  print(blackPink.asMap());
+  print(blackPink.toSet());
+
+  Map blackPinkMap = blackPink.asMap();
+
+  print(blackPinkMap.keys.toList());
+  print(blackPinkMap.values.toList());
+
+  Set blackPinkSet = Set.from(blackPink);
+  print(blackPinkSet.toList());
+}
---
+++ functional_programming/ch01_casting/test/ch01_casting_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch01_casting/ch01_casting.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
