3ebc98b7fe9f144d97fa321da30e73d35459abbd
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-21 15:25:01 +0900

    ch02_15_typedef

---
+++ dart_project/ch02_15_typedef/lib/ch02_15_typedef.dart
@@ -0,0 +1,3 @@
+int calculate(int i) {
+  return 6 * 7;
+}
---
+++ dart_project/ch02_15_typedef/test/ch02_15_typedef_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch02_15_typedef/ch02_15_typedef.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
