463a1cc4e06d50ad808e02bca58571427a8d8eab
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-21 15:10:50 +0900

    ch08_cascading_operator

---
+++ functional_programming/ch08_cascading_operator/test/ch08_cascading_operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch08_cascading_operator/ch08_cascading_operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
