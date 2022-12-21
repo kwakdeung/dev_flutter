dce08b16e09b5c3efab422379e313be3a41fffc0
kwakdeung <rhkrwnsdud22@naver.com>
2022-12-21 15:17:40 +0900

    ch02_14_enum

---
+++ dart_project/ch02_14_enum/bin/ch02_14_enum.dart
@@ -0,0 +1,19 @@
+// ch02_14_enum
+
+enum Status {
+  approved, // 승인
+  pending, // 대기
+  rejected, // 거절
+}
+
+void main(List<String> arguments) {
+  Status status = Status.approved;
+
+  if (status == Status.approved) {
+    print('승인입니다.');
+  } else if (status == Status.pending) {
+    print('대기입니다.');
+  } else {
+    print('거절입니다.');
+  }
+}
---
+++ dart_project/ch02_14_enum/test/ch02_14_enum_test.dart
@@ -0,0 +1,8 @@
+import 'package:ch02_14_enum/ch02_14_enum.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
